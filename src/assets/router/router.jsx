5f1971cd027f9fc7a1724a6a0031a35347471208
Neemias Vieira <neemias.vieira@live.com>
2023-04-25 13:19:53 -0300

    Teste nas rotas

--- src/assets/router/router.jsx
+++ src/assets/router/router.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import React from "react";
 import Home from "../../paginas/Home/Home";
 import Sobre from "../../paginas/Sobre/Sobre";
@@ -7,14 +7,14 @@ import Contato from "../../paginas/Contato/Contato";
 
 export function Router() {
     return(
-        <HashRouter>
+        <BrowserRouter basename="/InglesComGabi/">
             <Routes>
                 <Route index element={<Home/>}></Route>
                 <Route path = "/sobre" element={<Sobre/>}></Route>
                 <Route path = "/plataforma" element={<Plataforma/>}></Route>
                 <Route path = "/contato" element={<Contato/>}></Route>
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     )
 }
 
